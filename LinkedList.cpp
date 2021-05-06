b082a650546cde7b4ff55e5276063b03c284245a
Dacker15 <barbang.davide@gmail.com>
2021-05-06 15:50:08 +0200

    feat: WIP of LinkedList

---
+++ LinkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by barba on 06/05/2021.
+//
+
+#include "LinkedList.h"
---
+++ LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by barba on 06/05/2021.
+//
+
+#ifndef LINKEDLIST_LINKEDLIST_H
+#define LINKEDLIST_LINKEDLIST_H
+
+
+class LinkedList {
+private:
+};
+
+
+#endif //LINKEDLIST_LINKEDLIST_H
