a43e02de8eaea9179f816505c69bc647febba2c3
Davide <barbang.davide@gmail.com>
2021-05-08 19:44:51 +0200

    fix: Moved all in .h files && Fixed push_back and pop_back

--- LinkedList.h
+++ LinkedList.h
@@ -11,20 +11,106 @@ template<typename T>
 class LinkedList {
 private:
     Node<T>* head;
-    Node<T>* getHead();
-    void setHead(Node<T>* head);
-    Node<T>* getNode(int index);
-    void setNode(int index, Node<T>* node);
+    Node<T>* getHead() {
+        return this->head;
+    }
+    void setHead(Node<T>* head) {
+        this->head = head;
+    }
+    Node<T>* getNode(int index) {
+        Node<T>* pointer = this->getHead();
+        try {
+            for (int i = 0; i < index; i++) {
+                Node<T>* next = pointer->getNext();
+                if (next != nullptr) {
+                    pointer = next;
+                    if (i == index) return pointer;
+                } else {
+                    throw out_of_range("Element out of range");
+                }
+            }
+        } catch (out_of_range& e) {
+            cerr << e.what() << endl;
+        }
+    }
+    void setNode(int index, Node<T>* node) {
+        Node<T>* pointer = this->getHead();
+        try {
+            for (int i = 0; i < index; i++) {
+                auto next = pointer->getNext();
+                if (next != nullptr) {
+                    pointer = next;
+                    if (i == index) pointer = node;
+                } else {
+                    throw out_of_range("Element out of range");
+                }
+            }
+        } catch (out_of_range& e) {
+            cerr << e.what() << endl;
+        }
+    }
 public:
-    LinkedList();
-    T at(int index);
-    void at(int index, T value);
-    bool empty();
-    void pop_back();
-    void push_back(T value);
-    void remove(int index);
-    int size();
-    T operator[](int index);
+    LinkedList() {
+        this->setHead(nullptr);
+    }
+    T at(int index) {
+        Node<T>* node = this->getNode(index);
+        return node->getValue();
+    }
+    void at(int index, T value) {
+        Node<T>* node = this->getNode(index);
+        auto* next = new Node<T>(value);
+        node->setNext(next);
+        next->setPrev(node);
+        next->setNext(node->getNext());
+    }
+    bool empty() {
+        return this->getHead() == nullptr;
+    }
+    void pop_back() {
+        Node<T>* pointer = this->getHead();
+        if (pointer != nullptr) {
+            auto last = pointer->getPrev();
+            if (last == pointer) {
+                delete this->getHead();
+            } else {
+                auto prePrev = last->getPrev();
+                prePrev->setNext(pointer);
+                pointer->setPrev(prePrev);
+                delete last;
+            }
+        }
+    }
+    void push_back(T value) {
+        Node<T>* pointer = this->getHead();
+        auto node = new Node<T>(value);
+        if (pointer != nullptr) {
+            auto last = pointer->getPrev();
+            node->setNext(pointer);
+            node->setPrev(last);
+            last->setNext(node);
+        } else {
+            node->setNext(node);
+            node->setPrev(node);
+            this->setHead(node);
+        }
+    }
+    void remove(int index) {
+        Node<T>* node = this->getNode(index);
+    }
+    int size() {
+        int size = 0;
+        Node<T>* pointer = this->getHead();
+        if (pointer == nullptr) return size;
+        while (pointer->getNext() != nullptr) {
+            pointer = pointer->getNext();
+            size++;
+        }
+        return size;
+    }
+    T operator[](int index) {
+        return this->at(index);
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-    Node<int> node = Node<int>(10);
+    LinkedList<double> list = LinkedList<double>();
+    cout << list.empty() << endl;
+    cout << list.size() << endl;
+    list.push_back(1);
+    list.push_back(2);
+    cout << list.size() << endl;
     return 0;
 }
